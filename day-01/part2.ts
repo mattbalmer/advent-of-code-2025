1f19afb84db452c436cafd6a11129eb366a661c5
Matt Balmer <signup@mattbalmer.com>
2025-12-01 14:10:40 +0100

    part2

--- day-01/part2.ts
+++ day-01/part2.ts
@@ -1,5 +1,38 @@
 import { Execute } from './format';
 
+const START = 50;
+const UPPER = 100;
+
+const dialUp = (n: number, a: number): [number, number] => {
+  const v = (n + a) % UPPER;
+  const whole = Math.floor(a / UPPER);
+  const partial = v < n && v !== 0 ? 1 : 0;
+  const r = whole + partial;
+  return [r, v];
+}
+
+const dialDown = (n: number, a: number): [number, number] => {
+  const v = (UPPER + ((n + a) % UPPER)) % UPPER
+  const whole = Math.floor(Math.abs(a) / UPPER);
+  const partial = v > n && n !== 0 ? 1 : 0;
+  const r = whole + partial;
+  return [r, v];
+}
+
 export const execute: Execute = (lines) => {
-  return 4;
+  let count = 0;
+  let value = START;
+
+  for(const [side, amount] of lines) {
+    const sign = side === 'L' ? -1 : 1;
+    let [r, v] = side === 'R' ? dialUp(value, amount * sign) : dialDown(value, amount * sign);
+    count += r;
+    value = v;
+
+    if (value === 0) {
+      count++;
+    }
+  }
+
+  return count;
 }
--- day-01/test.ts
+++ day-01/test.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import { execute, getData } from '@utils/data';
 import { format } from './format';
 import * as part1 from './part1';
-// import * as part2 from './part2';
+import * as part2 from './part2';
 
 const { TEST_DATA, DATA } = getData(
   1
@@ -25,18 +25,18 @@ describe(`Day 1`, () => {
     });
   });
 
-  // describe('part 2', () => {
-  //   it('should work on test case', () => {
-  //     const expected = 0;
-  //     const result = execute(part2, TEST_DATA, format);
-  //
-  //     expect(result).to.equal(expected);
-  //   });
-  //
-  //   // it('should give the real answer', () => {
-  //   //   const result = execute(part2, DATA, format);
-  //   //
-  //   //   console.log(result);
-  //   // });
-  // });
+  describe('part 2', () => {
+    it('should work on test case', () => {
+      const expected = 6;
+      const result = execute(part2, TEST_DATA, format);
+
+      expect(result).to.equal(expected);
+    });
+
+    it('should give the real answer', () => {
+      const result = execute(part2, DATA, format);
+
+      console.log(result);
+    });
+  });
 });
